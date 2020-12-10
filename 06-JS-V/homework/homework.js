2ffe2ff17ee7f4a48ffa35a42ad4e48beb0fe132
marco <marcopablodemaio@gmail.com>
2020-12-09 23:50:52 -0300

    listo el inciso 2, falta hacer el 1 (teoria)

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -9,12 +9,28 @@ function crearUsuario() {
   // Devuelve la clase
   // Tu código:
 
+  function Usuario(opciones){
+    this.usuario=opciones.usuario;
+    this.nombre=opciones.nombre;
+    this.email=opciones.email;
+    this.password=opciones.password;
+  }
+  Usuario.prototype.saludar = function(){
+    return 'Hola, mi nombre es '+this.nombre;
+  }
+
+  return Usuario;
+
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+
+  Constructor.prototype.saludar = function(){
+    return 'Hello World!';
+  }
 
 }
 
@@ -25,7 +41,20 @@ function agregarStringInvertida() {
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
 
+  String.prototype.reverse = function(){
+
+    var vuelta = '';
+    for (var i=0 ; i<this.length ; i++){
+      vuelta = this.charAt(i) + vuelta;
+    }
+    return vuelta;
+  };
+
 }
+
+
+
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
